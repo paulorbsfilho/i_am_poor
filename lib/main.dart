25e8eb3587ed935228ee957de8324a8012a3d8e8
paulorbsfilho <p4ulinho@outlook.com>
2020-05-18 17:54:24 -0300

    Finalização

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
       home: Scaffold(
         appBar: new AppBar(
           title: new Text('I Am Poor'),
